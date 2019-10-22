f877d08e40a61522c8d8a745c9c52327930d9905
Elvio <elviost@gmail.com>
2019-10-22 10:52:24 +0200

    Fixed formatting and reduced LOC

--- opencl/vecsum_clean.c
+++ opencl/vecsum_clean.c
@@ -5,7 +5,7 @@
 #define CL_TARGET_OPENCL_VERSION 120
 #include "ocl_boiler.h"
 
-size_t gws_align_init;		// Struct per fare le cose meglio
+size_t gws_align_init;	// Struct per fare le cose meglio
 size_t gws_align_sum;		// global variables bad
 
 // I kernel restituiscono sempre void
@@ -14,7 +14,7 @@ cl_event vecinit(cl_kernel vecinit_k, cl_command_queue que,
 				 cl_mem d_v1,  cl_mem d_v2, cl_int nels)
 {
 	// Allineamento del global work size
-	const size_t gws[] = {round_mul_up(nels,gws_align_init)};				// Sarà sempre un multiplo di gws_align
+	const size_t gws[] = {round_mul_up(nels,gws_align_init)};  			// Sarà sempre un multiplo di gws_align
 	printf("init gws: %d | %zu = %zu\n",nels, gws_align_init, gws[0]);
 	
 	cl_event vecinit_evt;
@@ -36,8 +36,8 @@ cl_event vecinit(cl_kernel vecinit_k, cl_command_queue que,
 cl_event vecsum(cl_kernel vecsum_k, cl_command_queue que, 
 				cl_mem d_vsum, cl_mem d_v1, cl_mem d_v2, cl_int nels, cl_event init_evt)
 {
-    // Allineamento del global work size
-	const size_t gws[] = {round_mul_up(nels,gws_align_sum)};	// Sarà sempre un multiplo di gws_align
+  // Allineamento del global work size
+	const size_t gws[] = {round_mul_up(nels,gws_align_sum)};  			// Sarà sempre un multiplo di gws_align
 	printf("sum gws: %d | %zu = %zu\n",nels, gws_align_sum, gws[0]);
 
 	cl_event vecsum_evt;
@@ -58,9 +58,6 @@ cl_event vecsum(cl_kernel vecsum_k, cl_command_queue que,
 	return vecsum_evt;
 }
 
-
-
-
 void verify(const int *vsum, int nels)
 {
 	for (int i = 0; i < nels; ++i)
@@ -71,7 +68,6 @@ void verify(const int *vsum, int nels)
         }
 }
 
-
 int main(int argc, char *argv[])
 {
 	if (argc <= 1) 
@@ -81,11 +77,11 @@ int main(int argc, char *argv[])
 	}
 
 	const int nels = atoi(argv[1]);
-	const size_t memsize = nels*sizeof(cl_int);	// su device è signed int (con complemento a due)
-												// su host è definito il cl_int, garantito che sia identico a quello del device (entro certi limiti)
-												// cl_float, cl_double, ...
+	const size_t memsize = nels*sizeof(cl_int);	
+	// su device è signed int (con complemento a due)
+	// su host è definito il cl_int, garantito che sia identico a quello del device (entro certi limiti)
 	
-    cl_platform_id plat_id = select_platform();
+  cl_platform_id plat_id = select_platform();
 	cl_device_id dev_id = select_device(plat_id);
 	cl_context ctx = create_context(plat_id, dev_id);
 	cl_command_queue que = create_queue(ctx, dev_id);
@@ -97,15 +93,17 @@ int main(int argc, char *argv[])
 	cl_kernel vecsum_k = clCreateKernel(prog, "vecsum", &err);
 	ocl_check(err, "create kernel vecsum");
 
-	err = clGetKernelWorkGroupInfo(vecinit_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(gws_align_init), &gws_align_init, NULL);
+	err = clGetKernelWorkGroupInfo(vecinit_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
+				sizeof(gws_align_init), &gws_align_init, NULL);
 	ocl_check(err, "Preferred wg multiple for init");
-	err = clGetKernelWorkGroupInfo(vecsum_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(gws_align_sum), &gws_align_sum, NULL);
+	err = clGetKernelWorkGroupInfo(vecsum_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
+				sizeof(gws_align_sum), &gws_align_sum, NULL);
 	ocl_check(err, "Preferred wg multiple for sum");
 
 	// d_ = per device, sanity naming per il programmatore
 	cl_mem d_v1 = NULL, d_v2 = NULL, d_vsum = NULL;
 
-	d_v1 = clCreateBuffer(ctx CL_MEM_READ_WRITE | CL_MEM_HOST_NO_ACCESS, memsize, NULL, &err);
+	d_v1 = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_HOST_NO_ACCESS, memsize, NULL, &err);
 	ocl_check(err, "create buffer d_v1");
 	
 	d_v2 = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_HOST_NO_ACCESS, memsize, NULL, &err);
@@ -127,8 +125,8 @@ int main(int argc, char *argv[])
 	sum_evt = vecsum(vecsum_k, que, d_vsum, d_v1, d_v2, nels, init_evt);
 	end_sum = clock();
 
-	//Rende visibile all'host (CPU) il contenuto di un buffer
-    // Non eseguire mai kernel mentre il buffer è mappato, undefined behaviour, idem se mappo su un ReadBuffer.
+	// Rende visibile all'host (CPU) il contenuto di un buffer
+  // Non eseguire mai kernel mentre il buffer è mappato, undefined behaviour, idem se mappo su un ReadBuffer.
 	cl_int * h_vsum = clEnqueueMapBuffer(que,d_vsum,CL_FALSE,CL_MAP_READ, 0,memsize,1,&sum_evt, &read_evt , &err);
 	clWaitForEvents(1, &read_evt);	// Garanzia che vecsum ha concluso l'operazione
 	verify(h_vsum, nels);
@@ -149,7 +147,7 @@ int main(int argc, char *argv[])
 		nels, runtime_read_ms, read_bw_gbs,nels/1.0e6/runtime_read_ms);
 
 	err = clEnqueueUnmapMemObject(que,d_vsum,h_vsum,0,NULL,NULL);
-    clReleaseMemObject(d_vsum);		// Rilascio per i buffer openCL, in questo caso blocchi contigui di memoria.
+  clReleaseMemObject(d_vsum);		// Rilascio per i buffer openCL, in questo caso blocchi contigui di memoria.
 	clReleaseMemObject(d_v1);
 	clReleaseMemObject(d_v2);
 	clReleaseKernel(vecsum_k);		
