57395c78ab1d3c17b1eef89409ad94257453c471
Elvio <elviost@gmail.com>
2019-10-22 12:57:25 +0200

    incomplete vector smoothing source code

---
+++ opencl/vecsmooth.c
@@ -0,0 +1,153 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define CL_TARGET_OPENCL_VERSION 120
+#include "ocl_boiler.h"
+
+size_t gws_align_init;	// Struct per fare le cose meglio
+size_t gws_align_smooth;		// global variables bad
+
+// I kernel restituiscono sempre void
+// Si deve specificare il tipo di memoria, i puntatori di solito risiedono in global memory
+cl_event vecinit(cl_kernel vecinit_k, cl_command_queue que, 
+				 cl_mem d_v1, cl_int nels)
+{
+	// Allineamento del global work size
+	const size_t gws[] = {round_mul_up(nels,gws_align_init)};  			// Sarà sempre un multiplo di gws_align
+	printf("init gws: %d | %zu = %zu\n",nels, gws_align_init, gws[0]);
+	
+	cl_event vecinit_evt;
+	cl_int err;
+
+	cl_uint i = 0;	//0, 1, 2 v
+	err = clSetKernelArg(vecinit_k, i++, sizeof(d_v1), &d_v1);
+	ocl_check(err, "set vecinit arg_dv1", i-1);
+	err = clSetKernelArg(vecinit_k, i++, sizeof(nels), &nels);
+	ocl_check(err, "set vecinit arg_nels", i-1);
+	
+	err = clEnqueueNDRangeKernel(que, vecinit_k, 1, NULL, gws, NULL, 0, NULL, &vecinit_evt);
+	ocl_check(err, "enqueue vecinit");
+	return vecinit_evt;
+}
+
+cl_event vecsmooth(cl_kernel vecsmooth_k, cl_command_queue que, 
+				cl_mem d_vsmooth, cl_mem d_v1, cl_int nels, cl_event init_evt)
+{
+  // Allineamento del global work size
+	const size_t gws[] = {round_mul_up(nels,gws_align_smooth)};  			// Sarà sempre un multiplo di gws_align
+	printf("smooth gws: %d | %zu = %zu\n",nels, gws_align_smooth, gws[0]);
+
+	cl_event vecsmooth_evt;
+	cl_int err;
+
+	cl_uint i = 0;
+	err = clSetKernelArg(vecsmooth_k, i++, sizeof(d_vsmooth), &d_vsmooth);
+	ocl_check(err, "set vecsmooth arg_dvsmooth", i-1);
+	err = clSetKernelArg(vecsmooth_k, i++, sizeof(d_v1), &d_v1);
+	ocl_check(err, "set vecsmooth arg_dv1", i-1);
+	err = clSetKernelArg(vecsmooth_k, i++, sizeof(nels), &nels);
+	ocl_check(err, "set vecsmooth arg_nels", i-1);
+
+	err = clEnqueueNDRangeKernel(que, vecsmooth_k, 1, NULL, gws, NULL, 1, &init_evt, &vecsmooth_evt);
+	ocl_check(err, "enqueue vecsmooth");
+	return vecsmooth_evt;
+}
+
+void verify(const int *vsmooth, int nels)
+{
+	for (int i = 0; i < nels; ++i)
+  {
+    int target = 1 == nels -1 ? i - 1 : 1;
+        if (vsmooth[i] != i) 
+        {
+            fprintf(stderr, "mismatch @ %d : %d != %d\n", i, vsmooth[i], nels);
+            exit(3);
+        }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 1) 
+	{
+		fprintf(stderr, "specify number of elements\n");
+		exit(1);
+	}
+
+	const int nels = atoi(argv[1]);
+	const size_t memsize = nels*sizeof(cl_int);	
+	// su device è signed int (con complemento a due)
+	// su host è definito il cl_int, garantito che sia identico a quello del device (entro certi limiti)
+	
+  cl_platform_id plat_id = select_platform();
+	cl_device_id dev_id = select_device(plat_id);
+	cl_context ctx = create_context(plat_id, dev_id);
+	cl_command_queue que = create_queue(ctx, dev_id);
+	cl_program prog = create_program("vecsmooth.ocl", ctx, dev_id);	// File dei kernel deve avere lo stesso identico nome
+	cl_int err;
+
+	cl_kernel vecinit_k = clCreateKernel(prog, "vecinit", &err);
+	ocl_check(err, "create kernel vecinit");
+	cl_kernel vecsmooth_k = clCreateKernel(prog, "vecsmooth", &err);
+	ocl_check(err, "create kernel vecsmooth");
+
+	err = clGetKernelWorkGroupInfo(vecinit_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
+				sizeof(gws_align_init), &gws_align_init, NULL);
+	ocl_check(err, "Preferred wg multiple for init");
+	err = clGetKernelWorkGroupInfo(vecsmooth_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
+				sizeof(gws_align_smooth), &gws_align_smooth, NULL);
+	ocl_check(err, "Preferred wg multiple for smooth");
+
+	// d_ = per device, sanity naming per il programmatore
+	cl_mem d_v1 = NULL, d_vsmooth = NULL;
+
+	d_v1 = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_HOST_NO_ACCESS, memsize, NULL, &err);
+	ocl_check(err, "create buffer d_v1");
+	
+	// Sappiamo che leggeremo i dati tramite map
+	d_vsmooth = clCreateBuffer(ctx, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY | CL_MEM_ALLOC_HOST_PTR, memsize, NULL, &err);
+	ocl_check(err, "create buffer d_vsmooth");
+
+	clock_t start_init, end_init;
+	clock_t start_smooth, end_smooth;
+	cl_event init_evt, smooth_evt, read_evt;
+
+	start_init = clock();
+	init_evt = vecinit(vecinit_k, que, d_v1, nels);
+	end_init = clock();
+
+	start_smooth = clock();
+	smooth_evt = vecsmooth(vecsmooth_k, que, d_vsmooth, d_v1, nels, init_evt);
+	end_smooth = clock();
+
+	// Rende visibile all'host (CPU) il contenuto di un buffer
+  // Non eseguire mai kernel mentre il buffer è mappato, undefined behaviour, idem se mappo su un ReadBuffer.
+	cl_int * h_vsmooth = clEnqueueMapBuffer(que,d_vsmooth,CL_FALSE,CL_MAP_READ, 0,memsize,1,&smooth_evt, &read_evt , &err);
+	clWaitForEvents(1, &read_evt);	// Garanzia che vecsmooth ha concluso l'operazione
+	verify(h_vsmooth, nels);
+
+	const double runtime_init_ms = runtime_ms(init_evt);
+	const double runtime_smooth_ms  = runtime_ms(smooth_evt );
+	const double runtime_read_ms = runtime_ms(read_evt);
+
+	const double init_bw_gbs = 1.0*memsize/1.0e6/runtime_init_ms;
+	const double smooth_bw_gbs = 4.0*memsize/1.0e6/runtime_smooth_ms; // Ignoriamo quello che succede all'ultimo elemento. 3 read, 1 write
+	const double read_bw_gbs = memsize/1.0e6/runtime_read_ms;
+
+	printf("init: %d int in %gms: %g GB/s %g GE/s\n",
+		nels, runtime_init_ms, init_bw_gbs, nels/1.0e6/runtime_init_ms);
+	printf("smooth : %d int in %gms: %g GB/s %g GE/s\n",
+		nels, runtime_smooth_ms, smooth_bw_gbs,nels/1.0e6/runtime_smooth_ms);
+	printf("read : %d int in %gms: %g GB/s %g GE/s\n",
+		nels, runtime_read_ms, read_bw_gbs,nels/1.0e6/runtime_read_ms);
+
+	err = clEnqueueUnmapMemObject(que,d_vsmooth,h_vsmooth,0,NULL,NULL);
+  clReleaseMemObject(d_vsmooth);		// Rilascio per i buffer openCL, in questo caso blocchi contigui di memoria.
+	clReleaseMemObject(d_v1);
+	clReleaseKernel(vecsmooth_k);		
+	clReleaseKernel(vecinit_k);		
+	clReleaseProgram(prog); 		
+	clReleaseCommandQueue(que);		
+	clReleaseContext(ctx);				
+}
