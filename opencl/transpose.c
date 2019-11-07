dc59a58361319524996dc8dcaf688b58e538faf2
Elvio <32675220+Darakuu@users.noreply.github.com>
2019-11-07 11:37:35 +0100

    Fixed matrix transpose code

--- opencl/transpose.c
+++ opencl/transpose.c
@@ -6,7 +6,7 @@
 #include "ocl_boiler.h"
 
 size_t gws_align_init;
-size_t gws_align_smooth;		
+size_t gws_align_transpose;		
 
 
 cl_event matinit(cl_kernel matinit_k, cl_command_queue que, 
@@ -29,10 +29,10 @@ cl_event matinit(cl_kernel matinit_k, cl_command_queue que,
 	return matinit_evt;
 }
 
-cl_event transpose(cl_kernel transpose_k, cl_command_queue que, 
+cl_event transpose(cl_kernel transpose_k, int vec, cl_command_queue que, 
 									cl_mem d_T, cl_mem d_A, cl_int nrows_T, cl_int ncols_T, cl_event init_evt)
 {
-	const size_t gws[] = {round_mul_up(ncols_T, gws_align_init), nrows_T};  			// Al contrario solo per mantenere la coalescenza
+	const size_t gws[] = {round_mul_up(ncols_T/vec, gws_align_init), nrows_T/vec};
 	cl_event trans_evt;
 	cl_int err;
 
@@ -51,16 +51,16 @@ cl_event transpose(cl_kernel transpose_k, cl_command_queue que,
 	return trans_evt;
 }
 
-void verify(const int *h_A, int nrows, int ncols)
+void verify(const int *h_T, int nrows_T, int ncols_T)
 {
-  for (int r = 0; r < nrows; ++r)
+  for (int r = 0; r < nrows_T; ++r)
   {
-    for (int c = 0; c < ncols; ++c)
+    for (int c = 0; c < ncols_T; ++c)
     {
-      if (h_A[r*ncols+c] != r-c)
+      if (h_T[r*ncols_T+c] != c-r)
       {
         fprintf(stderr, "mismatch = (%d, %d) : %d != %d\n",
-                r,c,h_A[r*ncols+c],r-c);
+                r,c,h_T[r*ncols_T+c],c-r);
                 exit(3);
       }
     }
@@ -76,41 +76,72 @@ int main(int argc, char *argv[])
 
 	const int nrows_A = atoi(argv[1]);
 	const int ncols_A = atoi(argv[2]);
-	const size_t memsize = nrows*ncols*sizeof(cl_int);
-  const int ncols_T = nrows_A;
+	const int vec = argc == 4 ? atoi(argv[3]) : 1;
+
+	const char *trans_kernel_name = NULL;
+
+	switch(vec)
+	{
+		case 1: trans_kernel_name = "transpose"; break;
+		case 2: trans_kernel_name = "transpose2"; break;
+		case 4: trans_kernel_name = "transpose4"; break;
+		default: 
+			fprintf(stderr, "vettorizzazioni supportate: 1 2 4\n");
+			exit(-1);
+	}
+
+	if (vec>1)
+	{
+		if (nrows_A & (vec-1))
+		{
+			fprintf(stderr, "nrows %d non è multiplo di %d\n",nrows_A,vec);
+			exit(1);
+		}
+		if (ncols_A & (vec-1))
+		{
+			fprintf(stderr, "ncols %d non è multiplo di %d\n",ncols_A,vec);
+			exit(1);
+		}
+	}
+
+	const size_t memsize = nrows_A*ncols_A*sizeof(cl_int);
   const int nrows_T = ncols_A;
+  const int ncols_T = nrows_A;
 
   cl_platform_id plat_id = select_platform();
 	cl_device_id dev_id = select_device(plat_id);
 	cl_context ctx = create_context(plat_id, dev_id);
 	cl_command_queue que = create_queue(ctx, dev_id);
-	cl_program prog = create_program("transpose.ocl", ctx, dev_id);	// File dei kernel deve avere lo stesso identico nome
+	cl_program prog = create_program("transpose.ocl", ctx, dev_id);
 	cl_int err;
 
-	cl_kernel transpose_k = clCreateKernel(prog, "transpose", &err);
+	cl_kernel matinit_k = clCreateKernel(prog, "matinit", &err);
+	ocl_check(err, "create kernel matinit");
+	cl_kernel transpose_k = clCreateKernel(prog, trans_kernel_name, &err);
 	ocl_check(err, "create kernel transpose");
 
-	err = clGetKernelWorkGroupInfo(transpose_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
+	err = clGetKernelWorkGroupInfo(matinit_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
 				sizeof(gws_align_init), &gws_align_init, NULL);
+	err = clGetKernelWorkGroupInfo(transpose_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
+				sizeof(gws_align_transpose), &gws_align_transpose, NULL);
 	ocl_check(err, "Preferred wg multiple for init");
 
 	// d_ = per device, sanity naming per il programmatore
 	cl_mem d_A = NULL, d_T = NULL;
 
 	d_A = clCreateBuffer(ctx, CL_MEM_READ_WRITE | CL_MEM_HOST_NO_ACCESS, memsize, NULL, &err);
-	ocl_check(err, "create buffer d_v1");
+	ocl_check(err, "create buffer d_A");
   d_T = clCreateBuffer(ctx, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, memsize, NULL, &err);
-	ocl_check(err, "create buffer d_v1");
+	ocl_check(err, "create buffer d_T");
 
 	cl_event init_evt, trans_evt, read_evt;
 
-	init_evt = matinit(transpose_k, que, d_A, nrows_A, ncols_A);
-	trans_evt = transpose(transpose_k, que, d_T, d_A, nrows_T, ncols_T, init_evt);
-	//smooth_evt = vecsmooth(vecsmooth_k, que, lws, d_vsmooth, d_v1, nels, init_evt);
+	init_evt = matinit(matinit_k, que, d_A, nrows_A, ncols_A);
+	trans_evt = transpose(transpose_k, vec, que, d_T, d_A, nrows_T, ncols_T, init_evt);
 	
-	cl_int * h_T = clEnqueueMapBuffer(que,d_vsmooth,CL_FALSE,CL_MAP_READ, 0,memsize,1,&smooth_evt, &read_evt , &err);
-	clWaitForEvents(1, &read_evt);	// Garanzia che vecsmooth ha concluso l'operazione
-	verify(h_T, nrows,ncols);
+	cl_int * h_T = clEnqueueMapBuffer(que,d_T,CL_FALSE,CL_MAP_READ, 0,memsize,1,&trans_evt, &read_evt , &err);
+	clWaitForEvents(1, &read_evt);
+	verify(h_T, nrows_T,ncols_T);
 
 	const double runtime_init_ms = runtime_ms(init_evt);
   const double runtime_trans_ms = runtime_ms(trans_evt);
@@ -121,16 +152,16 @@ int main(int argc, char *argv[])
 	const double read_bw_gbs = memsize/1.0e6/runtime_read_ms;
 
 	printf("init: %dx%d int in %gms: %g GB/s %g GE/s\n",
-		nrows, ncols, runtime_init_ms, init_bw_gbs, nrows_A*ncols_A/1.0e6/runtime_init_ms);
+		nrows_A, ncols_A, runtime_init_ms, init_bw_gbs, nrows_A*ncols_A/1.0e6/runtime_init_ms);
     printf("transpose: %dx%d int in %gms: %g GB/s %g GE/s\n",
-		nrows, ncols, runtime_init_ms, init_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_init_ms);
+		nrows_T, ncols_T, runtime_init_ms, init_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_init_ms);
 	printf("read: %dx%d int in %gms: %g GB/s %g GE/s\n",
-		nrows, ncols, runtime_read_ms, read_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_read_ms);
+		nrows_T, ncols_T, runtime_read_ms, read_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_read_ms);
 
-	err = clEnqueueUnmapMemObject(que,d_A,h_A,0,NULL,NULL);
-	ocl_check(err, "unmap matrix");
-  clReleaseMemObject(d_A);		// Rilascio per i buffer openCL, in questo caso blocchi contigui di memoria.
+	err = clEnqueueUnmapMemObject(que,d_T,h_T,0,NULL,NULL);
+	ocl_check(err, "unmap T");
 	clReleaseMemObject(d_T);
+  clReleaseMemObject(d_A);
 	clReleaseKernel(transpose_k);		
 	clReleaseKernel(matinit_k);		
 	clReleaseProgram(prog); 		
