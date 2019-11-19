c20772c5c576a6f169b0db1f0dbc70821091e674
Elvio <32675220+Darakuu@users.noreply.github.com>
2019-11-19 16:24:21 +0100

    Collapsed all image tranpose src files into one to be closer to the course, and also for better maintainability. Fixed bugs and added 2D lws

--- opencl/images/transpose_img.c
+++ opencl/images/transpose_img.c
@@ -4,14 +4,11 @@
 #define CL_TARGET_OPENCL_VERSION 120
 #include "../ocl_boiler.h"
 
-size_t gws_align_init;
-size_t gws_align_transpose;		
-
-
-cl_event matinit(cl_kernel matinit_k, cl_command_queue que, 
-									cl_mem d_A, cl_int nrows, cl_int ncols)
+cl_event matinit(cl_kernel matinit_k, const size_t *lws, cl_command_queue que, 
+	cl_mem d_A, cl_int nrows, cl_int ncols)
 {
-	const size_t gws[] = {round_mul_up(ncols, gws_align_init), nrows};  			// Al contrario solo per mantenere la coalescenza
+	const size_t gws[] = {round_mul_up(ncols, lws[0]), 
+		round_mul_up(nrows, lws[1])};
 	cl_event matinit_evt;
 	cl_int err;
 
@@ -23,15 +20,33 @@ cl_event matinit(cl_kernel matinit_k, cl_command_queue que,
 	err = clSetKernelArg(matinit_k, i++, sizeof(nrows), &nrows);
 	ocl_check(err, "set matinit arg_nrows", i-1);
 	
-	err = clEnqueueNDRangeKernel(que, matinit_k, 2, NULL, gws, NULL, 0, NULL, &matinit_evt);
+	err = clEnqueueNDRangeKernel(que, matinit_k, 2, NULL, gws, lws, 0, NULL, &matinit_evt);
 	ocl_check(err, "enqueue matinit");
 	return matinit_evt;
 }
 
-cl_event transpose(cl_kernel transpose_k, cl_command_queue que, 
-									cl_mem d_T, cl_mem d_I, cl_int nrows_T, cl_int ncols_T, cl_event init_evt)
+cl_event imginit(cl_kernel imginit_k, const size_t *lws, cl_command_queue que,
+	cl_mem d_I, cl_int nrows, cl_int ncols)
 {
-	const size_t gws[] = {round_mul_up(ncols_T, gws_align_init), nrows_T};
+	const size_t gws[] = {round_mul_up(ncols,lws[0]), 
+		round_mul_up(nrows,lws[1])};
+	cl_event imginit_evt;
+	cl_int err;
+
+	cl_uint i=0;
+	err= clSetKernelArg(imginit_k, i++, sizeof(d_I), &d_I);
+	ocl_check(err,"set imginit arg dI");
+
+	err= clEnqueueNDRangeKernel(que, imginit_k, 2, NULL, gws, lws, 0, NULL, &imginit_evt);
+	ocl_check(err,"enqueue imginit");
+	return imginit_evt;
+}
+
+cl_event transpose(cl_kernel transpose_k, const size_t *lws, cl_command_queue que, 
+	cl_mem d_T, cl_mem d_I, cl_int nrows_T, cl_int ncols_T, cl_event init_evt)
+{
+	const size_t gws[] = {round_mul_up(ncols_T, lws[0]), 
+		round_mul_up(nrows_T, lws[1])};
 	cl_event trans_evt;
 	cl_int err;
 
@@ -42,7 +57,7 @@ cl_event transpose(cl_kernel transpose_k, cl_command_queue que,
 	ocl_check(err, "set tranpose arg_dI", i-1);
 	// Tecnicamente non servono righe e colonne perché sono nella struct
 
-	err = clEnqueueNDRangeKernel(que, transpose_k, 2, NULL, gws, NULL, 1, &init_evt, &trans_evt);
+	err = clEnqueueNDRangeKernel(que, transpose_k, 2, NULL, gws, lws, 1, &init_evt, &trans_evt);
 	ocl_check(err, "enqueue transpose");
 	return trans_evt;
 }
@@ -61,13 +76,14 @@ void verify(const int *h_T, int nrows_T, int ncols_T)
 
 int main(int argc, char *argv[])
 {
-	if (argc <= 2) {
-		fprintf(stderr, "specify number of rows and columns\n");
+	if (argc <= 4) {
+		fprintf(stderr, "specify number of rows and columns and lws 0,1\n");
 		exit(1);
 	}
 
 	const int nrows_A = atoi(argv[1]);
 	const int ncols_A = atoi(argv[2]);
+	const size_t lws[2] = { atoi(argv[3]), atoi(argv[4])};
 
 	const char *trans_kernel_name = "transpose_img";
 
@@ -84,14 +100,10 @@ int main(int argc, char *argv[])
 
 	cl_kernel matinit_k = clCreateKernel(prog, "matinit", &err);
 	ocl_check(err, "create kernel matinit");
+	cl_kernel imginit_k = clCreateKernel(prog, "imginit", &err);
+	ocl_check(err, "create kernel imginit");
 	cl_kernel transpose_k = clCreateKernel(prog, trans_kernel_name, &err);
 	ocl_check(err, "create kernel transpose");
-
-	err = clGetKernelWorkGroupInfo(matinit_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
-				sizeof(gws_align_init), &gws_align_init, NULL);
-	err = clGetKernelWorkGroupInfo(transpose_k, dev_id, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, 
-				sizeof(gws_align_transpose), &gws_align_transpose, NULL);
-	ocl_check(err, "Preferred wg multiple for init");
 
 	cl_mem d_A = NULL, d_T = NULL;
 	cl_mem d_I = NULL; // image
@@ -114,8 +126,8 @@ int main(int argc, char *argv[])
 	d_I = clCreateImage(ctx, CL_MEM_READ_WRITE | CL_MEM_HOST_NO_ACCESS,&fmt,&desc,NULL,&err);
 	ocl_check(err, "create image d_I");
 
-	cl_event init_evt, copy_evt, trans_evt, read_evt;	// copy_evt = inefficiente
-	init_evt = matinit(matinit_k, que, d_A, nrows_A, ncols_A);
+	cl_event init_evt, copy_evt, imginit_evt, trans_evt, read_evt;	// copy_evt = inefficiente
+	init_evt = matinit(matinit_k, lws, que, d_A, nrows_A, ncols_A);
 	
 	const size_t img_origin[3] = {0,0,0};
 	const size_t img_region[3] = {ncols_A, nrows_A, 1};
@@ -127,38 +139,44 @@ int main(int argc, char *argv[])
 		1, &init_evt, &copy_evt
 		);
 	ocl_check(err, "Copy d_A in d_I");
-
-	trans_evt = transpose(transpose_k, que, d_T, d_I, nrows_T, ncols_T, copy_evt);
+	imginit_evt = imginit(imginit_k, lws, que, d_I, nrows_A, ncols_A);
+	trans_evt = transpose(transpose_k, lws, que, d_T, d_I, nrows_T, ncols_T, copy_evt);
 	
 	cl_int * h_T = clEnqueueMapBuffer(que,d_T,CL_FALSE,CL_MAP_READ, 0,memsize,1,&trans_evt, &read_evt , &err);
 	clWaitForEvents(1, &read_evt);
 	verify(h_T, nrows_T,ncols_T);
 
 	const double runtime_init_ms = runtime_ms(init_evt);
-	const double runtime_copy_ms = runtime_ms(trans_evt);
+	const double runtime_copy_ms = runtime_ms(copy_evt);
+	const double runtime_imginit_ms = runtime_ms(imginit_evt);
   const double runtime_trans_ms = runtime_ms(trans_evt);
 	const double runtime_read_ms = runtime_ms(read_evt);
 
 	const double init_bw_gbs = 1.0*memsize/1.0e6/runtime_init_ms;
 	const double copy_bw_gbs = 2.0*memsize/1.0e6/runtime_init_ms;
-  const double trans_bw_gbs = 1.0*memsize/1.0e6/runtime_init_ms;
+	const double imginit_bw_gbs = 1.0*memsize/1.0e6/runtime_imginit_ms;
+  const double trans_bw_gbs = 2.0*memsize/1.0e6/runtime_init_ms;
 	const double read_bw_gbs = memsize/1.0e6/runtime_read_ms;
 
-	printf("init: %dx%d int in %gms: %g GB/s %g GE/s\n",
-		nrows_A, ncols_A, runtime_init_ms, init_bw_gbs, nrows_A*ncols_A/1.0e6/runtime_init_ms);
-  printf("copy: %dx%d int in %gms: %g GB/s %g GE/s\n",
-		nrows_T, ncols_T, runtime_copy_ms, copy_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_copy_ms);
-	printf("transpose: %dx%d int in %gms: %g GB/s %g GE/s\n",
-		nrows_T, ncols_T, runtime_init_ms, init_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_init_ms);
-	printf("read: %dx%d int in %gms: %g GB/s %g GE/s\n",
-		nrows_T, ncols_T, runtime_read_ms, read_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_read_ms);
+	printf("init: %dx%d int, lws[%ld*%ld], in %gms: %g GB/s %g GE/s\n",
+		nrows_A, ncols_A, lws[0], lws[1], runtime_init_ms, init_bw_gbs, nrows_A*ncols_A/1.0e6/runtime_init_ms);
+  printf("copy: %dx%d int, lws[%ld*%ld], in %gms: %g GB/s %g GE/s\n",
+		nrows_T, ncols_T, lws[0], lws[1], runtime_copy_ms, copy_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_copy_ms);
+	printf("imginit: %dx%d int, lws[%ld*%ld], in %gms: %g GB/s %g GE/s\n",
+		nrows_A, ncols_A, lws[0], lws[1], runtime_imginit_ms, imginit_bw_gbs, nrows_A*ncols_A/1.0e6/runtime_imginit_ms);
+	printf("transpose: %dx%d int, lws[%ld*%ld], in %gms: %g GB/s %g GE/s\n",
+		nrows_T, ncols_T, lws[0], lws[1], runtime_init_ms, init_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_init_ms);
+	printf("read: %dx%d int, lws[%ld*%ld], in %gms: %g GB/s %g GE/s\n",
+		nrows_T, ncols_T, lws[0], lws[1], runtime_read_ms, read_bw_gbs, nrows_T*ncols_T/1.0e6/runtime_read_ms);
 
 	err = clEnqueueUnmapMemObject(que,d_T,h_T,0,NULL,NULL);
 	ocl_check(err, "unmap T");
+	clReleaseMemObject(d_I);
 	clReleaseMemObject(d_T);
   clReleaseMemObject(d_A);
 	clReleaseKernel(transpose_k);
-	clReleaseKernel(matinit_k);	
+	clReleaseKernel(imginit_k);	
+	clReleaseKernel(matinit_k);
 	clReleaseProgram(prog);	
 	clReleaseCommandQueue(que);
 	clReleaseContext(ctx);		
